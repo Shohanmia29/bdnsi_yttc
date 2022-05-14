63c20d8665289ccfae78ce915a070545865ab061
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-05-14 13:04:38 +0600

    Add access management to navigation

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="url(config('laratrust.panel.path'))" :active="request()->is(config('laratrust.panel.path').'/*')">
+                        {{ __('Access Management') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -65,6 +68,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="url(config('laratrust.panel.path'))" :active="request()->is(config('laratrust.panel.path').'/*')">
+                {{ __('Access Management') }}
             </x-responsive-nav-link>
         </div>
 
