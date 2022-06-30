c10257fcbbcadff187c38635a0d0ebd2654b8fef
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-06-30 16:35:28 +0600

    Cleanup

--- routes/web.php
+++ routes/web.php
@@ -19,26 +19,20 @@ Route::get('/', function () {
 
 Route::get('/dashboard', \App\Http\Controllers\DashboardController::class)->middleware(['auth'])->name('dashboard');
 
-<<<<<<< HEAD
 Route::resource('password-update', \App\Http\Controllers\PasswordUpdateController::class)
     ->only(['create','store']);
 Route::resource('profile-update', \App\Http\Controllers\ProfileUpdateController::class)
     ->only(['create','store']);
-=======
+
 Route::middleware('auth')->group(function(){
     Route::resource('password-update', \App\Http\Controllers\PasswordUpdateController::class)
         ->only(['create','store']);
     Route::resource('profile-update', \App\Http\Controllers\ProfileUpdateController::class)
         ->only(['create','store']);
-    Route::resource('deposit', \App\Http\Controllers\DepositController::class)->only(['index','create','store']);
-    Route::resource('transfer', \App\Http\Controllers\TransferController::class)->only(['index','create', 'store']);
-    Route::get('wallet-log/{wallet}', \App\Http\Controllers\WalletLogController::class)->name('wallet-log.index');
 });
 
 Route::get('portal/{user}', \App\Http\Controllers\PortalController::class)->name('portal');
 
-
->>>>>>> 528c622 (Add portal to admin)
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
