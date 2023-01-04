12976aa75866a7b527508dfc0bc8590442c4d331
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2023-01-04 13:09:57 +0600

    Update banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="w-full">
         <div class="mx-auto flex w-full max-w-4xl">
-            <img src="{{ asset('images/banner.jpg') }}" alt="" class="min-h-48 w-full"/>
+            <img src="{{ asset('images/banner2.jpeg') }}" alt="" class="min-h-48 w-full"/>
         </div>
     </div>
 </x-guest-layout>
