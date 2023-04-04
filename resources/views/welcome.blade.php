15e3faa2b60c7cd2ac1429b87533dc13b22eb327
rksumone <sumon.npi@gmail.com>
2023-04-04 01:51:34 -0700

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,8 @@
     <div class="w-full sm:">
         <div class="mx-auto flex flex-col-reverse sm:flex-row w-full max-w-6xl sm: w-full ">
             <div class="w-2/3 mt-20 sm: w-2/2 mx-auto">
-                <div class="w-full text-3xl text-blue-600 pb-1"> Welcome to </div>
-                <span class="text-2xl">Website</span>
+                <div class="w-full text-3xl text-[#076e37] pb-1"> Welcome to </div>
+                <span class="text-2xl text-[#eb1c24]">BTSI</span>
                 <hr class="mt-2 ">
                 <p class="pt-5">With the help of the Government BICTF has built up greatest science and information technology, vocational based education in every Union, Thana, District and the main intention of it is to create the employment of the unemployed people through Information and Technology based research and training. Almost every sector in a country i..</p>
                 <button type="button" class=" mt-5 float-right pt-2 mt-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2  mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">READ MORE</button>
