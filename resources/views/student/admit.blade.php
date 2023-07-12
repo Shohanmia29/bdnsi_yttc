c0c3f28bdfc8e3059fc389732378413af584ef78
shohan29 <shohanur7777@gmail.com>
2023-07-12 16:30:42 +0600

    admit card added

--- resources/views/student/admit.blade.php
+++ resources/views/student/admit.blade.php
@@ -21,16 +21,16 @@
         }
     </style>
     <div class="w-full mt-8 bg-white   ">
-        <div class="r-bg relative" style="background-image: url({{asset('images/admit.png')}})">
+        <div class="r-bg relative text-gray-500" style="background-image: url({{asset('images/admit.png')}})">
             <div class="absolute top-[23.3%] left-[12.5%] w-38 text-xs font-bold text-left">{{$student->id??''}}</div>
-            <div class="absolute top-[30.3%] left-[19.5%] w-38 text-xs font-bold text-left">{{$student->center_id??''}}</div>
+            <div class="absolute top-[31%] left-[19.5%] w-38 text-xs font-bold text-left">{{$student->center_id??''}}</div>
             <div class="absolute top-[38%] left-[19.5%] w-38 text-xs font-bold text-left">{{$student->roll??''}}</div>
             <div class="absolute top-[45.2%] left-[19.5%] w-38 text-xs font-bold text-left">{{$student->registration??''}}</div>
             <div class="absolute top-[37.5%] left-[62.3%] w-38 text-xs font-bold text-left">{{$student->center->name??''}}</div>
-            <div class="absolute top-[45.5%] left-[62.5%] w-38 text-xs font-bold text-left">{{$student->course->name??''}}ssss</div>
+            <div class="absolute top-[45.5%] left-[62.5%] w-38 text-xs font-bold text-left">Computer Office Application</div>
             <div class="absolute top-[51.5%] left-[19.5%] w-38 text-xs font-bold text-left">{{$student->session->name??''}}</div>
-            <div class="absolute top-[51.5%] left-[62.5%] w-38 text-xs font-bold text-left">{{$student->course->name??''}}ssss</div>
-            <div class="absolute top-[59.5%] left-[19.5%] w-38 text-xs font-bold text-left">{{$student->name??''}}</div>
+            <div class="absolute top-[51.5%] left-[62.5%] w-38 text-xs font-bold text-left">{{$student->name??''}}</div>
+            <div class="absolute top-[59.5%] left-[19.5%] w-38 text-xs font-bold text-left">Regular</div>
             <div class="absolute top-[59%] left-[62.5%] w-38 text-xs font-bold text-left">{{$student->fathers_name??''}}</div>
             <div class="absolute top-[67.5%] left-[19.5%] w-38 text-xs font-bold text-left">{{\Carbon\Carbon::now()}}</div>
             <div class="absolute top-[67.5%] left-[62.5%] w-38 text-xs font-bold text-left">{{$student->fathers_name??''}}</div>
