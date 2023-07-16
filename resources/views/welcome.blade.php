5b11d12377d572c08ddd5eb0c114d0913f119e87
shohan29 <shohanur7777@gmail.com>
2023-07-16 10:49:35 +0600

    readmore about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,21 +21,44 @@
     </div>
 
     <div class="w-full sm:">
-        <div class="mx-auto flex flex-col-reverse sm:flex-row w-full max-w-6xl sm: w-full ">
+        <div class="mx-auto flex flex-col-reverse sm:flex-row w-full max-w-6xl sm: w-full " x-data="{ showFullText: false }" x-cloak>
             <div class="w-2/3 mt-20 sm: w-2/2 mx-auto">
                 <div class="w-full text-3xl text-[#076e37] pb-1"> Welcome to </div>
                 <span class="text-2xl text-[#eb1c24]">BTSI</span>
                 <hr class="mt-2 ">
-                <p class="pt-5">{{\App\Models\ConfigDictionary::get('about_us')??''}}</p>
-                <button type="button" class=" mt-5 float-right pt-2 mt-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2  mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">READ MORE</button>
+                <div >
+                    <p>
+                        <span x-show="!showFullText"
+                              x-transition:enter="transition ease-out duration-300"
+                              x-transition:enter-start="opacity-0 scale-90"
+                              x-transition:enter-end="opacity-100 scale-100"
+                              x-transition:leave="transition ease-in duration-300"
+                              x-transition:leave-start="opacity-100 scale-100"
+                              x-transition:leave-end="opacity-0 scale-90"
+
+                        > {{\Illuminate\Support\Str::limit(\App\Models\ConfigDictionary::get('about_us'),200,$end='...')}}</span>
+
+                     <p
+                        x-show="showFullText"
+                        class="pt-5"
+                        x-transition:enter="transition ease-out duration-300"
+                        x-transition:enter-start="opacity-0 scale-90"
+                        x-transition:enter-end="opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-300"
+                        x-transition:leave-start="opacity-100 scale-100"
+                        x-transition:leave-end="opacity-0 scale-90"
+                    >{{\App\Models\ConfigDictionary::get('about_us')}}
+                    </p>
+
+                    </p>
+                </div>
+                <button @click="showFullText = !showFullText" type="button" class=" mt-5 float-right pt-2 mt-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2  mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">READ MORE</button>
             </div>
             <div class="mt-20 sm: mt-5">
                 <img class="h-auto max-w-full pl-8" src="{{asset('/logo.png')}}" alt="image description">
             </div>
         </div>
     </div>
-
-
     <section id="course">
         <div class="w-full bg-[#F3F4F6] pb-5">
             <div class="mx-auto flex w-full max-w-6xl ">
