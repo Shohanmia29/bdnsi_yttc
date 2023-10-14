f877688baecf8edca7b6341a2e0eb2175fbd4856
shohan29 <shohanur7777@gmail.com>
2023-10-14 14:10:26 +0600

    fix admin card

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -31,6 +31,9 @@
                                 <th   class="px-6 py-3">
                                     Email
                                 </th>
+                                <th   class="px-6 py-3">
+                                    Password Change
+                                </th>
                             </tr>
                             </thead>
                             <tbody>
@@ -41,6 +44,13 @@
                                     </th>
                                     <td class="px-6 py-4">
                                         {{$user->email??''}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a class="cursor-pointer" href="{{route('admin.adminList.edit',$user->id)}}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 119 0v3.75M3.75 21.75h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H3.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                            </svg>
+                                        </a>
                                     </td>
                                 </tr>
                             @endforeach
