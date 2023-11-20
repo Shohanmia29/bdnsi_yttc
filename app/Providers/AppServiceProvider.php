f40cd63a43f8c2b21da0912519d3d9874f803ced
shohan29 <shohanur7777@gmail.com>
2023-11-20 12:50:46 +0600

    defualt key length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Mixin\ResponseMixin;
 use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Schema::defaultStringLength(191);
+
         ResponseFactory::mixin(new ResponseMixin());
         Blade::directive('selected', function($expression){
             return "<?php echo ($expression) ? 'selected' : ''; ?>";
