1f924973d0e23e8f4986410b622a9fa9649b3741
shohan29 <shohanur7777@gmail.com>
2023-12-10 17:52:29 +0600

    fix :

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -143,13 +143,16 @@
 <section>
     <p class="flex justify-center py-2 px-4 text-sm sm:text-base">BTSI © 2022 All rights reserved. Developed by <a class="ml-1 " href="http://www.mmitsoft.com" target="_blank"> MMIT SOFT LTD</a></p>
 </section>
-
+{{$script??''}}
 <script src="{{mix('js/app.js')}}"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+
+
+
 
 <script>
     $(document).ready(function () {
