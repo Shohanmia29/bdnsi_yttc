f411865023eeb3da39f4703c310daa03baa3ae86
shohan29 <shohanur7777@gmail.com>
2023-12-28 12:25:27 +0600

    fix

--- resources/views/admin/student/registrationForm.blade.php
+++ resources/views/admin/student/registrationForm.blade.php
@@ -20,22 +20,20 @@
     <div class="  mt-8 bg-white p-3 print:p-0 min-w-[800px] overflow-x-auto">
           <div class="w-full print:hidden flex justify-end"><button onclick="window.print()" class="px-3 py-1 rounded-md bg-green-600 text-white ">Print</button></div>
           <div class="r-bg relative w-full   h-[130vh] print:h-[90vh] text-lg font-bold " style="background-image: url({{asset('images/register.jpg')}})">
-              <div class="absolute top-[16%] left-[17%]  w-38 ">{{$student->id??''}}</div>
-              <div class="absolute top-[20%] left-[40%]  w-38 text-2xl">{{$student->subject->name??''}}</div>
-              <div class="absolute top-[25.5%]  left-[35.8%]  w-38 ">{{$student->registration??'N/A'}}</div>
-              <div class="absolute top-[31%] left-[35.8%]  w-38 ">{{$student->roll??'N/A'}}</div>
-              <div class="absolute top-[36.5%] left-[35.8%]  w-38 ">{{$student->session->name??'N/A'}}</div>
-              <div class="absolute top-[25.5%] left-[75.7%] w-[19.5%] h-[17.8%]  ">
+
+              <div class="absolute top-[19%] left-[70.7%] w-[19.5%] h-[17.8%]  ">
                   <img class="w-full h-full border-2 border-black rounded-[2.3rem]" src="{{$student->picture??'N/A'}}" alt="">
               </div>
-          <div class="absolute top-[42%] left-[35.8%]  w-38 ">{{$student->name??'N/A'}}</div>
-          <div class="absolute top-[47.5%] left-[35.8%]  w-38 ">{{$student->fathers_name??'N/A'}}</div>
-          <div class="absolute top-[53%] left-[35.8%]  w-38 ">{{$student->mothers_name??'N/A'}}</div>
-          <div class="absolute top-[58.5%] left-[35.8%]  w-38 ">{{\App\Enums\Gender::fromValue(  $student->gender??'N/A')->key}}</div>
-          <div class="absolute top-[64%] left-[35.8%]  w-38 ">{{$student->present_address??'N/A'}}</div>
-          <div class="absolute top-[69.7%] left-[35.8%]  w-38 ">{{$student->permanent_address??'N/A'}}</div>
-          <div class="absolute top-[75.2%] left-[35.8%]  w-38 ">{{$student->center->name??'N/A'}}</div>
-          <div class="absolute top-[80.7%] left-[35.8%]  w-38 ">{{$student->center->code??'N/A'}}</div>
+          <div class="absolute top-[33%] left-[45.8%]  w-38 ">{{$student->name??'N/A'}}</div>
+          <div class="absolute top-[36.8%] left-[45.8%]  w-38 ">{{$student->fathers_name??'N/A'}}</div>
+          <div class="absolute top-[40.8%] left-[45.8%]  w-38 ">{{$student->mothers_name??'N/A'}}</div>
+          <div class="absolute top-[44.5%] left-[45.8%]  w-38 ">{{\App\Enums\Gender::fromValue(  $student->gender??'N/A')->key}}</div>
+           <div class="absolute top-[48.2%] left-[45.8%]  w-38 ">{{$student->center->name??'N/A'}}/{{$student->center->code??'N/A'}}</div>
+
+          <div class="absolute top-[52%] left-[45.8%]  w-38 ">{{$student->present_address??'N/A'}}</div>
+          <div class="absolute top-[55.7%] left-[45.8%]  w-38 ">{{$student->permanent_address??'N/A'}}</div>
+              <div class="absolute top-[59.2%]  left-[45.8%]  w-38 ">{{$student->registration??'N/A'}}</div>
+              <div class="absolute top-[62.5%] left-[45.8%]  w-38 ">{{$student->session->name??'N/A'}}</div>
           <div class="absolute top-[82.5%] left-[84.3%]  w-38 "><img class="w-14 h-10" src="{{asset('signature.png')}}" alt=""></div>
       </div>
     </div>
