7895ada5f3d3d1f8b323a653ddad9243cd5688c7
shohan29 <shohanur7777@gmail.com>
2023-12-28 13:26:52 +0600

    fix admit card

--- resources/views/admin/student/registrationForm.blade.php
+++ resources/views/admin/student/registrationForm.blade.php
@@ -11,30 +11,30 @@
         </div>
     </x-slot>
     <style>
-          .r-bg{
-              background-size: 100% 100%;
-              background-repeat: no-repeat;
-              background-position: center center;
-          }
+        .r-bg{
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+            background-position: center center;
+        }
     </style>
     <div class="  mt-8 bg-white p-3 print:p-0 min-w-[800px] overflow-x-auto">
-          <div class="w-full print:hidden flex justify-end"><button onclick="window.print()" class="px-3 py-1 rounded-md bg-green-600 text-white ">Print</button></div>
-          <div class="r-bg relative w-full   h-[130vh] print:h-[90vh] text-lg font-bold " style="background-image: url({{asset('images/register.jpg')}})">
+        <div class="w-full print:hidden flex justify-end"><button onclick="window.print()" class="px-3 py-1 rounded-md bg-green-600 text-white ">Print</button></div>
+        <div class="r-bg relative w-full   h-[130vh] print:h-[90vh] text-lg font-bold " style="background-image: url({{asset('images/register.jpg')}})">
 
-              <div class="absolute top-[19%] left-[70.7%] w-[19.5%] h-[17.8%]  ">
-                  <img class="w-full h-full border-2 border-black rounded-[2.3rem]" src="{{$student->picture??'N/A'}}" alt="">
-              </div>
-          <div class="absolute top-[33%] left-[45.8%]  w-38 ">{{$student->name??'N/A'}}</div>
-          <div class="absolute top-[36.8%] left-[45.8%]  w-38 ">{{$student->fathers_name??'N/A'}}</div>
-          <div class="absolute top-[40.8%] left-[45.8%]  w-38 ">{{$student->mothers_name??'N/A'}}</div>
-          <div class="absolute top-[44.5%] left-[45.8%]  w-38 ">{{\App\Enums\Gender::fromValue(  $student->gender??'N/A')->key}}</div>
-           <div class="absolute top-[48.2%] left-[45.8%]  w-38 ">{{$student->center->name??'N/A'}}/{{$student->center->code??'N/A'}}</div>
+            <div class="absolute top-[19%] left-[70.7%] w-[19.5%] h-[17.8%]  ">
+                <img class="w-full h-full border-2 border-black rounded-[2.3rem]" src="{{$student->picture??'N/A'}}" alt="">
+            </div>
+            <div class="absolute top-[33%] left-[45.8%]  w-38 ">{{$student->name??'N/A'}}</div>
+            <div class="absolute top-[36.8%] left-[45.8%]  w-38 ">{{$student->fathers_name??'N/A'}}</div>
+            <div class="absolute top-[40.8%] left-[45.8%]  w-38 ">{{$student->mothers_name??'N/A'}}</div>
+            <div class="absolute top-[44.5%] left-[45.8%]  w-38 ">{{\App\Enums\Gender::fromValue(  $student->gender??'N/A')->key}}</div>
+            <div class="absolute top-[48.2%] left-[45.8%] print:text-sm w-38 ">{{$student->center->name??'N/A'}}/{{$student->center->code??'N/A'}}</div>
 
-          <div class="absolute top-[52%] left-[45.8%]  w-38 ">{{$student->present_address??'N/A'}}</div>
-          <div class="absolute top-[55.7%] left-[45.8%]  w-38 ">{{$student->permanent_address??'N/A'}}</div>
-              <div class="absolute top-[59.2%]  left-[45.8%]  w-38 ">{{$student->registration??'N/A'}}</div>
-              <div class="absolute top-[62.5%] left-[45.8%]  w-38 ">{{$student->session->name??'N/A'}}</div>
-          <div class="absolute top-[82.5%] left-[84.3%]  w-38 "><img class="w-14 h-10" src="{{asset('signature.png')}}" alt=""></div>
-      </div>
+            <div class="absolute top-[52%] left-[45.8%]  print:text-sm w-38 ">{{$student->present_address??'N/A'}}</div>
+            <div class="absolute top-[55.7%] left-[45.8%] print:text-sm  w-38 ">{{$student->permanent_address??'N/A'}}</div>
+            <div class="absolute top-[59.2%]  left-[45.8%]  w-38 ">{{$student->registration??'N/A'}}</div>
+            <div class="absolute top-[62.5%] left-[45.8%]  w-38 ">{{$student->session->name??'N/A'}}</div>
+            <div class="absolute top-[82.5%] left-[84.3%]  w-38 "><img class="w-14 h-10" src="{{asset('signature.png')}}" alt=""></div>
+        </div>
     </div>
 </x-admin-app-layout>
--- resources/views/student/admit.blade.php
+++ resources/views/student/admit.blade.php
@@ -21,14 +21,13 @@
  <div class="print:min-h-screen">
      <div class="w-full   bg-white min-w-[800px] overflow-x-auto  ">
          <div class="flex flex-col   print:h-screen  justify-between">
-             <div class="r-bg  print:h-[48vh] h-screen   w-full relative text-[16px] font-bold text-gray-500" style="background-image: url({{asset('images/admit.jpg')}});font-family: 'Times New Roman'">
+             <div class="r-bg  print:h-[48vh] h-screen   w-full relative text-[16px] font-bold text-gray-500" style="background-image: url({{asset('images/admit.jpeg')}});font-family: 'Times New Roman'">
                  <div class="absolute top-[18.3%] left-[81.5%] w-38  ">
                      <img class="w-24 h-24 rounded-md" src="{{$student->picture}}" alt="">
                  </div>
-                 <div class="absolute top-[7%] left-[30.5%] text-[#1C77B9]      text-xl     w-38      uppercase">Bangladesh Technical & Software Institute</div>
-                 <div class="absolute top-[12.3%]  left-[30.5%] text-[#1C77B9] text-sm py-1 uppercase  tracking-[3px]     w-38  tracking-y-[20px]   ">{{$student->subject->name  .'-'.   date('y',strtotime($student->session->start_date))??''}}</div>
-                 <div class="absolute top-[23.3%] md:top-[22%] left-[12.5%] w-38  ">{{$student->id??''}}</div>
-                 <div class="absolute md:top-[18%] top-[19%] left-[56.5%] w-38  ">C-{{$student->center->code??''}}/{{date('y',strtotime($student->session->start_date))}} </div>
+
+                 <div class="absolute top-[26%]   left-[10.5%] w-38  ">{{$student->id??''}}</div>
+                 <div class="absolute   top-[19%] left-[52%] w-38  ">C-{{$student->center->code??''}}/{{date('y',strtotime($student->session->start_date))}} </div>
                  <div class="absolute top-[31%] left-[19.5%] w-38  ">{{$student->center->code??''}}</div>
                  <div class="absolute top-[38%] left-[19.5%] w-38  ">{{$student->roll??''}}</div>
                  <div class="absolute top-[45.2%] left-[19.5%] w-38  ">{{$student->registration??''}}</div>
@@ -42,14 +41,13 @@
                  <div class="absolute top-[67.5%] left-[62.5%] w-38  ">{{$student->mothers_name??''}}</div>
                  <div class="absolute top-[85.5%] text-[#7CA299]  left-[11%] w-38 capitalize tracking-[.1em] ">{{$student->center->name??''}}</div>
              </div>
-             <div class="r-bg  print:h-[48vh] h-screen   w-full relative text-[16px] font-bold text-gray-500" style="background-image: url({{asset('images/admit.png')}});font-family: 'Times New Roman'">
-                 <div class="absolute top-[18.3%] left-[81.5%] w-38 ">
+             <div class="r-bg  print:h-[48vh] h-screen   w-full relative text-[16px] font-bold text-gray-500" style="background-image: url({{asset('images/admit.jpeg')}});font-family: 'Times New Roman'">
+                 <div class="absolute top-[18.3%] left-[81.5%] w-38  ">
                      <img class="w-24 h-24 rounded-md" src="{{$student->picture}}" alt="">
                  </div>
-                 <div class="absolute top-[7%] left-[30.5%] text-[#1C77B9]      text-xl     w-38      uppercase">Bangladesh Technical & Software Institute</div>
-                 <div class="absolute top-[12.3%]  left-[30.5%] text-[#1C77B9] text-sm py-1 uppercase  tracking-[3px]     w-38  tracking-y-[20px]   ">{{$student->subject->name  .'-'.   date('y',strtotime($student->session->start_date))??''}}</div>
-                 <div class="absolute top-[23.3%] md:top-[22%] left-[12.5%] w-38  ">{{$student->id??''}}</div>
-                 <div class="absolute md:top-[18%] top-[19%] left-[56.5%] w-38  ">C-{{$student->center->code??''}}/{{date('y',strtotime($student->session->start_date))}} </div>
+
+                 <div class="absolute top-[26%]   left-[10.5%] w-38  ">{{$student->id??''}}</div>
+                 <div class="absolute   top-[19%] left-[52%] w-38  ">C-{{$student->center->code??''}}/{{date('y',strtotime($student->session->start_date))}} </div>
                  <div class="absolute top-[31%] left-[19.5%] w-38  ">{{$student->center->code??''}}</div>
                  <div class="absolute top-[38%] left-[19.5%] w-38  ">{{$student->roll??''}}</div>
                  <div class="absolute top-[45.2%] left-[19.5%] w-38  ">{{$student->registration??''}}</div>
