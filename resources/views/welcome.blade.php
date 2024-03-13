7b0407251617a9764eacd83dd9fdcf8f7411ee8b
shohan29 <shohanur7777@gmail.com>
2024-03-13 12:32:58 +0600

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
   <div class="max-w-6xl mx-auto">
          <div class="w-full flex ">
                <button class="px-3 py-1 bg-blue-700 text-white rounded-md">Notice</button>
-             <marquee class="font" behavior="" direction="">
+             <marquee class="font-bold" behavior="" direction="">
                  ২১‌শে ফেব্রুয়া‌রি পালন সংক্রান্ত নানা রকম সরকা‌রি বি‌ধি নিষেধ পালনের জরু‌রি অ‌ফিস আদেশ
              </marquee>
          </div>
