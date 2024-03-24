468cd1595f8ede951081bbae7ee2dae2daecf098
shohan29 <shohanur7777@gmail.com>
2024-03-24 11:18:09 +0600

    fix

--- resources/views/student/admit.blade.php
+++ resources/views/student/admit.blade.php
@@ -18,56 +18,44 @@
             background-position: center center;
         }
     </style>
- <div class="print:min-h-screen">
-     <div class="w-full   bg-white min-w-[800px] overflow-x-auto  ">
-         <div class="flex flex-col   print:h-screen  justify-between">
-             <div class="r-bg  print:h-[48vh] h-screen   w-full relative text-[16px] font-bold text-gray-500" style="background-image: url({{asset('images/admit.png')}});font-family: 'Times New Roman'">
-                 <div class="absolute top-[18.3%] left-[81.5%] w-38  ">
-                     <img class="w-24 h-24 rounded-md" src="{{$student->picture}}" alt="">
-                 </div>
-                 <div class="absolute top-[7%] left-[30.5%] text-[#1C77B9]      text-xl     w-38      uppercase">Bangladesh Technical & Software Institute</div>
-                 <div class="absolute top-[12.3%]  left-[30.5%] text-[#1C77B9] text-sm py-1 uppercase  tracking-[3px]     w-38  tracking-y-[20px]   ">{{$student->subject->name  .'-'.   date('y',strtotime($student->session->start_date))??''}}</div>
-                 <div class="absolute top-[23.3%] md:top-[22%] left-[12.5%] w-38  ">{{$student->id??''}}</div>
-                 <div class="absolute md:top-[18%] top-[19%] left-[56.5%] w-38  ">C-{{$student->center->code??''}}/{{date('y',strtotime($student->session->start_date))}} </div>
-                 <div class="absolute top-[31%] left-[19.5%] w-38  ">{{$student->center->code??''}}</div>
-                 <div class="absolute top-[38%] left-[19.5%] w-38  ">{{$student->roll??''}}</div>
-                 <div class="absolute top-[45.2%] left-[19.5%] w-38  ">{{$student->registration??''}}</div>
-                 <div class="absolute top-[37.5%] left-[62.3%] w-38  ">{{$student->center->name??''}}</div>
-                 <div class="absolute top-[45.5%] md:top-[44%] left-[62.5%] w-38  ">{{$student->subject->name??''}}</div>
-                 <div class="absolute top-[51.5%]  left-[19.5%] w-38  ">{{$student->session->name??''}}</div>
-                 <div class="absolute top-[51.5%] left-[62.5%] w-38  ">{{$student->name??''}}</div>
-                 <div class="absolute top-[59.5%] left-[19.5%] w-38  ">Regular</div>
-                 <div class="absolute top-[59.4%] md:top-[59%] left-[62.5%] w-38  ">{{$student->fathers_name??''}}</div>
-                 <div class="absolute top-[67.5%] left-[19.5%] w-38  ">{{$student->exam_date??''}}</div>
-                 <div class="absolute top-[67.5%] left-[62.5%] w-38  ">{{$student->mothers_name??''}}</div>
-                 <div class="absolute top-[85.5%] text-[#7CA299]  left-[11%] w-38 capitalize tracking-[.1em] ">{{$student->center->name??''}}</div>
-             </div>
-             <div class="r-bg  print:h-[48vh] h-screen   w-full relative text-[16px] font-bold text-gray-500" style="background-image: url({{asset('images/admit.png')}});font-family: 'Times New Roman'">
-                 <div class="absolute top-[18.3%] left-[81.5%] w-38 ">
-                     <img class="w-24 h-24 rounded-md" src="{{$student->picture}}" alt="">
-                 </div>
-                 <div class="absolute top-[7%] left-[30.5%] text-[#1C77B9]      text-xl     w-38      uppercase">Bangladesh Technical & Software Institute</div>
-                 <div class="absolute top-[12.3%]  left-[30.5%] text-[#1C77B9] text-sm py-1 uppercase  tracking-[3px]     w-38  tracking-y-[20px]   ">{{$student->subject->name  .'-'.   date('y',strtotime($student->session->start_date))??''}}</div>
-                 <div class="absolute top-[23.3%] md:top-[22%] left-[12.5%] w-38  ">{{$student->id??''}}</div>
-                 <div class="absolute md:top-[18%] top-[19%] left-[56.5%] w-38  ">C-{{$student->center->code??''}}/{{date('y',strtotime($student->session->start_date))}} </div>
-                 <div class="absolute top-[31%] left-[19.5%] w-38  ">{{$student->center->code??''}}</div>
-                 <div class="absolute top-[38%] left-[19.5%] w-38  ">{{$student->roll??''}}</div>
-                 <div class="absolute top-[45.2%] left-[19.5%] w-38  ">{{$student->registration??''}}</div>
-                 <div class="absolute top-[37.5%] left-[62.3%] w-38  ">{{$student->center->name??''}}</div>
-                 <div class="absolute top-[45.5%] md:top-[44%] left-[62.5%] w-38  ">{{$student->subject->name??''}}</div>
-                 <div class="absolute top-[51.5%]  left-[19.5%] w-38  ">{{$student->session->name??''}}</div>
-                 <div class="absolute top-[51.5%] left-[62.5%] w-38  ">{{$student->name??''}}</div>
-                 <div class="absolute top-[59.5%] left-[19.5%] w-38  ">Regular</div>
-                 <div class="absolute top-[59.4%] md:top-[59%] left-[62.5%] w-38  ">{{$student->fathers_name??''}}</div>
-                 <div class="absolute top-[67.5%] left-[19.5%] w-38  ">{{$student->exam_date??''}}</div>
-                 <div class="absolute top-[67.5%] left-[62.5%] w-38  ">{{$student->mothers_name??''}}</div>
-                 <div class="absolute top-[85.5%] text-[#7CA299]  left-[11%] w-38 capitalize tracking-[.1em] ">{{$student->center->name??''}}</div>
-             </div>
+    <div class=" ">
+        <div class="w-full    bg-white  overflow-x-auto  ">
+            <div class="">
+                <div class="r-bg  min-h-[100vh] print:min-h-[50vh] flex-col justify-center   w-full relative text-[16px] font-bold text-gray-500" style="background-image: url({{asset('images/admit.png')}});font-family: 'Times New Roman'">
+                    <div class="absolute top-[22.3%] left-[81.5%] w-38 ">
+                        <img class="w-24 h-24 rounded-md" src="{{$student->picture}}" alt="">
+                    </div>
+                    <div class="absolute top-[42.3%] left-[81.5%] w-38 ">
+                        <div id="qrcode_1"></div>
 
-         </div>
-         <div class="w-full flex justify-end py-1 print:hidden">
-             <button onClick="window.print()" type="button" class="px-2 py-1 bg-green-700 text-white rounded-lg ">Print</button>
-         </div>
-     </div>
- </div>
+                    </div>
+
+                    <div class="absolute top-[23.3%]   print:top-[22%]  left-[12.5%] w-38  ">{{$student->id??''}}</div>
+                    <div class="absolute top-[34%] left-[35.5%] w-38  ">{{$student->center->code??''}}</div>
+                    <div class="absolute top-[39%] left-[35.5%] w-38  ">{{$student->center->name??''}}</div>
+                    <div class="absolute top-[44%] left-[35.5%] w-38  ">{{$student->subject->name??''}}</div>
+                    <div class="absolute top-[50%] left-[35.5%] w-38  ">{{$student->fathers_name??''}}</div>
+                    <div class="absolute top-[55%] left-[35.5%] w-38  ">{{$student->fathers_name??''}}</div>
+                    <div class="absolute top-[60%] left-[35.5%] w-38  ">{{$student->roll??''}}</div>
+                    <div class="absolute top-[66%] left-[35.5%] w-38  ">{{$student->registration??''}}</div>
+                </div>
+
+            </div>
+            <div class="w-full flex justify-end py-1 print:hidden">
+                <button onClick="window.print()" type="button" class="px-2 py-1 bg-green-700 text-white rounded-lg ">Print</button>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="{{ asset('js/qrcode.js') }}"></script>
+    <script type="text/javascript">
+        var qrcode = new QRCode(document.getElementById("qrcode_1"), {
+            text: "{{ $student->id }}",
+            width: 70,
+            height: 70,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H,
+        });
+    </script>
 </x-app-layout>
