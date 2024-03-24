fbd5eb183947f62ace69a4353d40eaf786f546f2
shohan29 <shohanur7777@gmail.com>
2024-03-24 12:45:33 +0600

    fix

--- resources/views/student/admit.blade.php
+++ resources/views/student/admit.blade.php
@@ -50,7 +50,7 @@
     <script type="text/javascript" src="{{ asset('js/qrcode.js') }}"></script>
     <script type="text/javascript">
         var qrcode = new QRCode(document.getElementById("qrcode_1"), {
-            text: "{{ $student->id }}",
+            text: "Name : {{$student->name}} \n Roll:{{$student->roll}}  ",
             width: 70,
             height: 70,
             colorDark: "#000000",
