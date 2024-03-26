a218a441e74535422a1b2d267ba4fc3955950325
shohan29 <shohanur7777@gmail.com>
2024-03-26 15:52:42 +0600

    fix

--- resources/views/components/institute.blade.php
+++ resources/views/components/institute.blade.php
@@ -3,12 +3,12 @@
 <div class="border-2 border-double border-gray-500 rounded-md">
     <div class="  overflow-hidden rounded-lg bg-white ">
         <img  src="{{$institute->photo??''}}" alt="image" class="w-full   h-64">
-        <div class="p-8 bg-gray-200 text-center ">
-            <h3>
+        <div class=" py-6 bg-gray-200 text-center ">
+            <div>
                 <a href="{{route('institute.details',$institute->id)}}" class="text-dark hover:text-primary mb-4 underline  text-lg font-semibold  ">
                     {{$institute->name??''}}
                 </a>
-            </h3>
+            </div>
 
 
         </div>
