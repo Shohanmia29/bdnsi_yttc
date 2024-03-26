01aa46475f6901ca32b919c7b42699ecca07ae5b
shohan29 <shohanur7777@gmail.com>
2024-03-26 14:23:15 +0600

    fix

--- resources/views/institute_details.blade.php
+++ resources/views/institute_details.blade.php
@@ -14,7 +14,18 @@
                         <ul class="w-full">
                             <li class="w-full border p-2 rounded-md mt-2 flex  ">
                                 <div class="font-bold w-[40%]">
-                                    Course Name
+                                    Email
+                                </div>
+                                <div class=" w-[10%]">
+                                    :
+                                </div>
+                                <div class=" w-[50%]">
+                                    {{$data->email??'N/A'}}
+                                </div>
+                            </li>
+                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
+                                <div class="font-bold w-[40%]">
+                                    Director Name
                                 </div>
                                 <div class=" w-[10%]">
                                     :
@@ -25,7 +36,18 @@
                             </li>
                             <li class="w-full border p-2 rounded-md mt-2 flex  ">
                                 <div class="font-bold w-[40%]">
-                                    Course Duration
+                                    Address
+                                </div>
+                                <div class=" w-[10%]">
+                                    :
+                                </div>
+                                <div class=" w-[50%]">
+                                    {{$data->address??'N/A'}}
+                                </div>
+                            </li>
+                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
+                                <div class="font-bold w-[40%]">
+                                    District
                                 </div>
                                 <div class=" w-[10%]">
                                     :
@@ -36,7 +58,7 @@
                             </li>
                             <li class="w-full border p-2 rounded-md mt-2 flex  ">
                                 <div class="font-bold w-[40%]">
-                                    Course Rate
+                                    Upazilla
                                 </div>
                                 <div class=" w-[10%]">
                                     :
@@ -45,28 +67,7 @@
 
                                 </div>
                             </li>
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Educational Qualification
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
 
-                                </div>
-                            </li>
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Course Details
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-
-                                </div>
-                            </li>
 
 
                         </ul>
