34fc59e7834fe1f90bf448f56ee893e24977a6ea
shohan29 <shohanur7777@gmail.com>
2024-06-09 16:03:28 +0600

    serial number fix

--- app/Lib/Helper.php
+++ app/Lib/Helper.php
@@ -5,8 +5,22 @@ namespace App\Lib;
 class Helper
 {
 
-    public static function serialNumber($id){
-        return str_pad($id, 6, '0', STR_PAD_LEFT);
+    public static function serialNumber($id) {
+
+
+        $number = '305071273';
+        $length = 9;
+
+        // Convert $id to string to handle numeric IDs
+        $id = (string)$id;
+
+        // Calculate how much padding is needed
+        $paddingLength = $length - strlen($id);
+
+        // Create the padding from $number and concatenate with $id
+        $paddedId = substr($number, 0, $paddingLength) . $id;
+
+        return $paddedId;
     }
 
 
