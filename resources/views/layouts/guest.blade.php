ca8ea3712df25981db6ffb41e13c26e756baf37c
shohan29 <shohanur7777@gmail.com>
2024-06-10 11:55:00 +0600

    fix navbar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <div class="">
-    <img class="md:h-[250px] w-full  " src="{{asset('frontend/newbanner.jpg')}}" alt="Logo">
+    <img class="md:h-[250px] w-full  " src="{{asset('frontend/banner.jpg')}}" alt="Logo">
 </div>
 
 <div class="w-full">
