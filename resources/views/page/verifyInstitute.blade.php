60146a8d818120d7cb1a437edaf04a90465f3e20
shohan29 <shohanur7777@gmail.com>
2024-06-11 14:56:20 +0600

    select upazila

--- resources/views/page/verifyInstitute.blade.php
+++ resources/views/page/verifyInstitute.blade.php
@@ -1,35 +1,37 @@
 <x-frontend-layouts>
-    <script defer src="{{mix('js/app.js')}}"></script>
     <div class="container" x-data="centerData">
-        <div class="branch-fillter-wrap mt-4 py-4">
+        <div class="branch-filter-wrap mt-4 py-4">
             <div class="text-center mb-3">
                 <h5>Filter Verified Branch</h5>
             </div>
-            <form action="{{route('verifiedInstitute')}}" method="get">
+            <form action="{{ route('verifiedInstitute') }}" method="get">
                 <div class="row gap-2 justify-content-center">
                     <div class="col-md-3">
-                        <select class="form-select" name="division" x-model="division" x-ref="division" required  aria-label="District select">
+                        <select class="form-select" name="division" x-model="division" required aria-label="Division select">
+                            <option value="" disabled selected>Select Division</option>
                             @foreach(\App\Lib\Geo::divisions() as $divisionId => $division)
                                 <option value="{{ $divisionId }}" @selected(old('division') == $divisionId)>{{ $division['name'] }}</option>
                             @endforeach
                         </select>
                     </div>
                     <div class="col-md-3">
-                        <select class="form-select"  x-model="district" x-model="district" x-ref="district" name="district" required>
-                            <template x-for="district in districts">
-                                <option x-bind:value="district.id" x-html="district.name"></option>
+                        <select class="form-select" name="district" x-model="district" required aria-label="District select">
+                            <option value="" disabled selected>Select District</option>
+                            <template x-for="district in districts" :key="district.id">
+                                <option :value="district.id" x-text="district.name"></option>
                             </template>
                         </select>
                     </div>
                     <div class="col-md-3">
-                        <select class="form-select" x-model="upazilla" name="upazilla" required>
-                            <template x-for="upazilla in upazillas">
-                                <option x-bind:value="upazilla.id" x-html="upazilla.name"></option>
+                        <select class="form-select" name="upazilla" x-model="upazilla" required aria-label="Upazilla select">
+                            <option value="" disabled selected>Select Upazilla</option>
+                            <template x-for="upazilla in upazillas" :key="upazilla.id">
+                                <option :value="upazilla.id" x-text="upazilla.name"></option>
                             </template>
                         </select>
                     </div>
                     <div class="col-md-2">
-                        <button class="btn btn-outline-success"  >Reset</button>
+                        <button type="reset" class="btn btn-outline-success" x-on:click="resetFilters">Reset</button>
                     </div>
                 </div>
             </form>
@@ -48,49 +50,72 @@
             @endforelse
         </div>
         <div class="w-100">
-              {{$centers->links()}}
+            {{ $centers->links() }}
         </div>
     </div>
 
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-
-
-
-
-    <x-slot name="script">
-        <script>
-            document.addEventListener('alpine:init', () => {
-                Alpine.data('centerData', () => ({
-                    division: @js(old('division')),
-                    district: @js(old('district')),
-                    districts: [],
-                    upazillas: [],
-                    init(){
-                        this.filterDistricts(this.division ?? this.$refs.division.value)
-                        this.$watch('division', this.filterDistricts.bind(this))
-                        this.$watch('district', this.filterUpazillas.bind(this))
-                    },
-                    filterDistricts(division){
-                        const districts = Object.entries(@js(\App\Lib\Geo::districts())).map(item => ({
-                            id: item[0],
-                            name: item[1].name,
-                            division_id: item[1].division_id,
-                        }))
-                        this.districts = districts.filter(district => district.division_id == division)
-                        setTimeout(() => {
-                            this.filterUpazillas(this.district ?? this.$refs.district.value)
-                        }, 10)
-                    },
-                    filterUpazillas(district){
-                        const upazillas = Object.entries(@js(\App\Lib\Geo::upazillas())).map(item => ({
-                            id: item[0],
-                            name: item[1].name,
-                            district_id: item[1].district_id,
-                        }))
-                        this.upazillas = upazillas.filter(upazilla => upazilla.district_id == district)
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('centerData', () => ({
+                division: '{{ old('division', '') }}',
+                district: '{{ old('district', '') }}',
+                upazilla: '{{ old('upazilla', '') }}',
+                districts: [],
+                upazillas: [],
+                init() {
+                    // Fetch and set districts based on the initial division
+                    if (this.division) {
+                        this.filterDistricts();
                     }
-                }))
-            })
-        </script>
-    </x-slot>
+
+                    // Fetch and set upazillas based on the initial district
+                    if (this.district) {
+                        this.filterUpazillas();
+                    }
+
+                    // Watchers for changes
+                    this.$watch('division', value => {
+                        this.district = '';
+                        this.upazilla = '';
+                        this.filterDistricts();
+                    });
+
+                    this.$watch('district', value => {
+                        this.upazilla = '';
+                        this.filterUpazillas();
+                    });
+                },
+                filterDistricts() {
+                    const allDistricts = @json(\App\Lib\Geo::districts());
+                    this.districts = Object.entries(allDistricts)
+                        .map(([id, district]) => ({
+                            id: id,
+                            name: district.name,
+                            division_id: district.division_id
+                        }))
+                        .filter(district => district.division_id == this.division);
+                },
+                filterUpazillas() {
+                    const allUpazillas = @json(\App\Lib\Geo::upazillas());
+                    this.upazillas = Object.entries(allUpazillas)
+                        .map(([id, upazilla]) => ({
+                            id: id,
+                            name: upazilla.name,
+                            district_id: upazilla.district_id
+                        }))
+                        .filter(upazilla => upazilla.district_id == this.district);
+                },
+                resetFilters() {
+                    this.division = '';
+                    this.district = '';
+                    this.upazilla = '';
+                    this.districts = [];
+                    this.upazillas = [];
+                }
+            }));
+        });
+    </script>
+
 </x-frontend-layouts>
