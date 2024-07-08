2c426f66716870550bd61addfdea885ea1defcb6
shohan29 <shohanur7777@gmail.com>
2024-07-08 16:45:18 +0600

    fix design

--- resources/views/components/student.blade.php
+++ resources/views/components/student.blade.php
@@ -3,11 +3,11 @@
 <div class="bg-white shadow-dashboard rounded-md" style="    border: 3px double black;">
     <a href="{{route('successStudentDetails',$student->id)}}">
         <div class="flex   justify-center items-center    ">
-            <img class="  w-full   h-64 " src="{{$student->picture??''}}" alt=""  >
+            <img class="  w-full    " style="height: 200px;" src="{{$student->picture??''}}" alt=""  >
 
         </div>
         <div class="p-3 text-center">
-            <h2 class="text-sm font-medium text-coolGray-900 font-bold underline" data-config-id="auto-txt-19-1">{{$student->name??''}}</h2>
+            <h2 class="text-sm font-medium text-coolGray-900 py-5 font-bold underline" data-config-id="auto-txt-19-1">{{$student->name??''}}</h2>
 
         </div>
     </a>
