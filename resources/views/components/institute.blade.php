7542a48ecf5557012ffcd30746cf8d5dd94e290f
shohan29 <shohanur7777@gmail.com>
2024-07-08 17:30:14 +0600

    fix

--- resources/views/components/institute.blade.php
+++ resources/views/components/institute.blade.php
@@ -10,7 +10,7 @@
         <img src="{{$institute->photo??''}}" class="card-img-top" alt="Director Photo" style="height: 200px;">
         <div class="card-body" style="background-color:#c7c7c7d1; ">
             <a style="color:black;" href="{{route('institute.details',$institute->id)}}">
-                <h5 class="card-title" style=""><a  class="underline font-bold" >{{$institute->name??''}}</a></h5>
+                <h5 class="card-title" style=""><a href="{{route('institute.details',$institute->id)}}"   class="underline font-bold" >{{$institute->name??''}}</a></h5>
             </a>
         </div>
     </div>
