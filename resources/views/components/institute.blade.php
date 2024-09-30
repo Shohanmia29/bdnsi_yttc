95bd5f344182ced31012a7724b054ce574edd746
shohan29 <shohanur7777@gmail.com>
2024-09-30 17:28:25 +0600

    fix

--- resources/views/components/institute.blade.php
+++ resources/views/components/institute.blade.php
@@ -7,7 +7,7 @@
 </style>
 <div class="  mt-4">
     <div class=" card   " style="border: 3px double black; height: 350px;">
-        <img src="{{$institute->photo??''}}" class="card-img-top" alt="Director Photo" style="height: 200px;">
+        <img src="{{$institute->director_image??''}}" class="card-img-top" alt="Director Photo" style="height: 200px;">
         <div class="card-body" style="background-color:#c7c7c7d1; ">
             <a style="color:black;" href="{{route('institute.details',$institute->id)}}">
                 <h5 class="card-title" style=""><a href="{{route('institute.details',$institute->id)}}"   class="underline font-bold" >{{$institute->name??''}}</a></h5>
