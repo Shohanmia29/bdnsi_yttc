5cea72f16053bfee54f4847607161d9ccc259fd1
shohanmia29 <shohanur7777@gmail.com>
2024-11-14 12:54:39 +0600

    fix

--- resources/views/page/verifyInstitute.blade.php
+++ resources/views/page/verifyInstitute.blade.php
@@ -58,6 +58,18 @@
     </div>
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $(document).ready(function () {
+            setTimeout(() => {
+                $('select').niceSelect('destroy');
+            }, 1000);
+
+
+        });
+
+    </script>
 
     <script>
         document.addEventListener('alpine:init', () => {
@@ -120,5 +132,7 @@
             }));
         });
     </script>
+
+
 
 </x-frontend-layouts>
