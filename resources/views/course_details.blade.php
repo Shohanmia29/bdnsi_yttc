9bb67509628af30147942a2f2b18b7310e779d6e
shohanmia29 <shohanur7777@gmail.com>
2024-11-25 17:50:02 +0600

    fix

--- resources/views/course_details.blade.php
+++ resources/views/course_details.blade.php
@@ -1,79 +1,44 @@
-<x-guest-layout>
+<x-frontend-layouts>
 
-<div class="mt-10">
-      <div class="max-w-6xl mx-auto px-3">
-
-            <div class="shadow-lg rounded-md border p-10">
-                   <div class="w-full flex flex-wrap">
-                          <div class="w-full md:w-[30%] p-3">
-                              <img class="w-full" src="{{$data->photo??''}}" alt="">
-                          </div>
-                          <div class="w-full md:w-[70%] p-3">
-                               <ul class="w-full">
-                                     <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                           <div class="font-bold w-[40%]">
-                                               Course Name
-                                           </div>
-                                            <div class=" w-[10%]">
-                                                :
-                                            </div>
-                                         <div class=" w-[50%]">
-                                             {{$data->name??'N/A'}}
-                                         </div>
-                                     </li>
-                                     <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                           <div class="font-bold w-[40%]">
-                                               Course Duration
-                                           </div>
-                                            <div class=" w-[10%]">
-                                                :
-                                            </div>
-                                         <div class=" w-[50%]">
-                                             {{$data->duration??'N/A'}}
-                                         </div>
-                                     </li>
-                                     <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                           <div class="font-bold w-[40%]">
-                                               Course Rate
-                                           </div>
-                                         <div class=" w-[10%]">
-                                             :
-                                         </div>
-
-                                         <div class=" w-[50%]">
-                                             {{$data->rate??'N/A'}}
-                                         </div>
-                                     </li>
-                                     <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                           <div class="font-bold w-[40%]">
-                                               Educational Qualification
-                                           </div>
-                                            <div class=" w-[10%]">
-                                                :
-                                            </div>
-                                         <div class=" w-[50%]">
-                                             {{$data->education_qualification??'N/A'}}
-                                         </div>
-                                     </li>
-                                     <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                           <div class="font-bold w-[40%]">
-                                               Course Details
-                                           </div>
-                                            <div class=" w-[10%]">
-                                                :
-                                            </div>
-                                         <div class=" w-[50%]">
-                                             {{$data->course_details??'N/A'}}
-                                         </div>
-                                     </li>
-
-
-                               </ul>
-
-                          </div>
-                   </div>
+    <div class="container mt-5">
+        <div class="card shadow-lg border-0 rounded">
+            <div class="row g-0">
+                <!-- Image Section -->
+                <div class="col-md-4 p-3">
+                    <img src="{{$data->photo ?? ''}}" class="img-fluid rounded" alt="Course Image">
+                </div>
+                <!-- Details Section -->
+                <div class="col-md-8 p-3">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-c text-white rounded d-flex align-items-center mb-2">
+                            <div class="fw-bold w-50">Course Name</div>
+                            <div class="text-center w-10">:</div>
+                            <div class="w-50">{{$data->name ?? 'N/A'}}</div>
+                        </li>
+                        <li class="list-group-item bg-c text-white rounded d-flex align-items-center mb-2">
+                            <div class="fw-bold w-50">Course Duration</div>
+                            <div class="text-center w-10">:</div>
+                            <div class="w-50">{{$data->duration ?? 'N/A'}}</div>
+                        </li>
+                        <li class="list-group-item bg-c text-white rounded d-flex align-items-center mb-2">
+                            <div class="fw-bold w-50">Course Rate</div>
+                            <div class="text-center w-10">:</div>
+                            <div class="w-50">{{$data->rate ?? 'N/A'}}</div>
+                        </li>
+                        <li class="list-group-item bg-c text-white rounded d-flex align-items-center mb-2">
+                            <div class="fw-bold w-50">Educational Qualification</div>
+                            <div class="text-center w-10">:</div>
+                            <div class="w-50">{{$data->education_qualification ?? 'N/A'}}</div>
+                        </li>
+                        <li class="list-group-item bg-c text-white rounded d-flex align-items-center">
+                            <div class="fw-bold w-50">Course Details</div>
+                            <div class="text-center w-10">:</div>
+                            <div class="w-50">{{$data->course_details ?? 'N/A'}}</div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-      </div>
-</div>
+        </div>
+    </div>
 
-</x-guest-layout>
+</x-frontend-layouts>
--- resources/views/page/successStudentDetails.blade.php
+++ resources/views/page/successStudentDetails.blade.php
@@ -1,161 +1,79 @@
 <x-frontend-layouts>
 
-    <div class="py-10">
-        <div class="max-w-6xl mx-auto px-3">
-
-            <div class="shadow-lg rounded-md border p-10">
-                <div class="w-full flex flex-wrap">
-                    <div class="w-full md:w-[30%] p-3">
-                        <img class="w-full" src="{{$data->picture??''}}" alt="">
+    <div class="bg-light">
+    <div class="py-5">
+        <div class="container">
+            <div class="card shadow-lg border-0 rounded-3">
+                <div class="row g-0">
+                    <!-- Image Section -->
+                    <div class="col-md-4 p-4 text-center">
+                        <img src="{{$data->picture ?? ''}}" class="img-fluid rounded-circle border" alt="Student Image" style="max-width: 200px;">
                     </div>
-                    <div class="w-full md:w-[70%] p-3">
-                         <div class="p-2 font-bold text-xl text-center md:text-2xl">
-                             Student information
-                         </div>
-                        <ul class="w-full">
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Registration No
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->registration??'N/A'}}
-                                </div>
+                    <!-- Details Section -->
+                    <div class="col-md-8 p-4">
+                        <h3 class="text-center text-orange-500 fw-bold mb-4">Student Information</h3>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Student Name</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->name ?? 'N/A'}}</div>
                             </li>
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Student Name
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->name??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Father Name</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->fathers_name ?? 'N/A'}}</div>
                             </li>
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                  Father Name
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->fathers_name??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Mother Name</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->mothers_name ?? 'N/A'}}</div>
                             </li>
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                   Mother Name
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->mothers_name??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Gender</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->gender->key ?? 'N/A'}}</div>
                             </li>
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                 Gender
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->gender->key??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Present Address</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->present_address ?? 'N/A'}}</div>
                             </li>
-
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Present address
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->present_address??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Student Thana</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->permanent_address ?? 'N/A'}}</div>
                             </li>
-
-                            <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Student Thana
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->permanent_address??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Student District</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->permanent_address ?? 'N/A'}}</div>
                             </li>
-
-                          <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Student District
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->permanent_address??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Institute Name</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->center->name ?? 'N/A'}}</div>
                             </li>
-                          <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                    Institute Name
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->center->name??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Course Name</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->subject->name ?? 'N/A'}}</div>
                             </li>
-                          <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                   Course Name
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->subject->name??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Course Duration</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->course_duration ?? 'N/A'}}</div>
                             </li>
-                          <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                   Course Duration
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->course_duration??'N/A'}}
-                                </div>
+                            <li class="list-group-item bg-c text-white rounded-2 mb-2 d-flex align-items-center">
+                                <div class="fw-bold w-50">Session</div>
+                                <div class="text-center w-10">:</div>
+                                <div class="w-50">{{$data->session->name ?? 'N/A'}}</div>
                             </li>
-                          <li class="w-full border p-2 rounded-md mt-2 flex  ">
-                                <div class="font-bold w-[40%]">
-                                   Session
-                                </div>
-                                <div class=" w-[10%]">
-                                    :
-                                </div>
-                                <div class=" w-[50%]">
-                                    {{$data->session->name??'N/A'}}
-                                </div>
-                            </li>
-
-
                         </ul>
-
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 </x-frontend-layouts>
