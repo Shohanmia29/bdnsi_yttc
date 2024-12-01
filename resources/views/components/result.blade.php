122ce8e6dd3b95f006cc51d9ab0430950df9523f
shohanmia29 <shohanur7777@gmail.com>
2024-12-01 14:34:22 +0600

    fix

--- resources/views/components/result.blade.php
+++ resources/views/components/result.blade.php
@@ -137,7 +137,7 @@
     <div class="w-full flex flex-wrap">
         <div class="w-full   print:w-3/5  ">
             <div class="w-full h3 py-2 text-xl md:text-4xl text-center font-bold">Bangladesh Youth Skills & Technical Training</div>
-            <div class="w-full py-2 text-center font-semibold">Govt. License No: C-195491</div>
+            <div class="w-full py-2 text-center font-semibold">RJSC NO: C-178431</div>
         </div>
     </div>
     <div class="w-full flex flex-wrap justify-center mt-3">
