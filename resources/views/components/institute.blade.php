a290c83733a5cd52f196570951b991d1a3d75370
shohanmia29 <shohanur7777@gmail.com>
2024-12-05 13:15:06 +0600

    fix

--- resources/views/components/institute.blade.php
+++ resources/views/components/institute.blade.php
@@ -7,11 +7,8 @@
     </div>
     <div class="school-commite-card-info">
         <h4 class="s-commite-member-name"><a href="{{route('institute.details',$institute->id)}}">{{$institute->name??''}}</a></h4>
-        <p class="s-commite-member-designation">(Commodi laborum Ame)</p>
-        <ul class="s-commite-card-info-list">
-            <li>সদস্য যোগদান: <span style="margin-left: 2px;">১৫ জুলাই, ১৯৭৮</span></li>
-            <li>সদস্য মেয়াদকাল: <span style="margin-left: 2px;">০২ নভেম্বর, ২০২৩</span></li>
-        </ul>
+        <p class="s-commite-member-designation">({{$institute->owner_name??''}})</p>
+
  {{--       <ul class="s-commite-card-social">
             <li>
                 <a href="tel:01702233333"><i class="icofont-ui-call"></i></a>
