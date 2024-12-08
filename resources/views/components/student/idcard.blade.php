4bf4ca56ad38b01c5345c5469af4e9d0af177e06
shohanmia29 <shohanur7777@gmail.com>
2024-12-08 12:00:11 +0600

    fix

--- resources/views/components/student/idcard.blade.php
+++ resources/views/components/student/idcard.blade.php
@@ -1,7 +1,27 @@
 @props(['student'])
+<script src="{{asset('js/pdf.js')}}"></script>
+<div class="flex items-center justify-center space-x-4 print:hidden mt-4">
+    <button
+        type="button"
+        class="px-4 py-2 rounded-md bg-green-800 text-white hover:bg-green-700 transition"
+        onclick="window.print()">
+        Print
+    </button>
+    <button
+        onclick="generate_pdf()"
+        class="px-4 py-2 rounded-md bg-green-800 text-white hover:bg-green-700 transition">
+        Download
+    </button>
+    <a
+        type="button"
+        class="px-4 py-2 rounded-md bg-green-800 text-white hover:bg-green-700 transition"
+        href="">
+        Back
+    </a>
+</div>
 
 <div class="md:p-10">
-    <div class="flex w-full flex-wrap">
+    <div class="flex w-full flex-wrap" id="html2pdf">
         <div class=" p-2 w-1/2">
             <div
                 class="relative h-[600px] w-full rounded-md p-2"
@@ -14,7 +34,7 @@
             >
                 <div class="absolute inset-0 flex items-center justify-center">
                     <div class="text-center">
-                        <img class="mx-auto h-24 w-24 rounded-full border-[4px] border-[#6aa84f]" src="https://t4.ftcdn.net/jpg/02/14/74/61/360_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg" alt="" />
+                        <img class="mx-auto h-28 w-28 rounded-full border-[4px] border-[#6aa84f]" src="{{$student->picture??''}}" alt="" />
 
                         <div class="mt-5">
                             <table class="mx-auto w-full font-semibold text-xs text-left">
@@ -67,6 +87,32 @@
 </div>
 
 
+<script type="text/javascript">
+    function generate_pdf() {
+        var opt = {
+            margin: 0,
+            filename: 'idcard.pdf',
+            image: {
+                type: 'jpeg',
+                quality: 0.99
+            },
+            html2canvas: {
+                scale: 1,
+                width: 1200,
+                height: 1660
+            },
+            jsPDF: {
+                unit: 'in',
+                format: 'A3',
+                orientation: 'portrait'
+            }
+        };
+        // html2pdf().from(document.getElementById('fullpage2')).set(opt).save();
+        html2pdf().from(document.getElementById('html2pdf')).set(opt).save().then(function() {
+            document.getElementById('fullpage2').classList.add('hidePDFdata');
+        });
+    }
+</script>
 
 
 
