8598f0685c57d99d5894e45c576b80ce19173425
shohanmia29 <shohanur7777@gmail.com>
2025-01-07 17:24:36 +0600

    fix

--- resources/views/admin/student/certificate2.blade.php
+++ resources/views/admin/student/certificate2.blade.php
@@ -1,49 +1,3 @@
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Certificate</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-
-</head>
-<body>
-<style>
-    @font-face {
-        font-family: certificate;
-        src: url({{asset('frontend/fonts/Monotype Corsiva/Monotype-Corsiva-Regular-Italic.ttf')}});
-    }
-    * {
-        font-family: certificate;
-    }
-
-    .bg-image {
-        background: url({{asset('images/student/certificate01.jpg')}});
-        background-size: contain;
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-</style>
-<div class="bg-image w-full h-screen relative font-bold text-base">
-    <p class="absolute top-[291px] lg:top-[294px] left-[340px] lg:left-[557px]">Serial No</p>
-    <p class="absolute top-[288px] lg:top-[288px] right-[90px] lg:right-[318px]">Registration No</p>
-    <p class="absolute top-[314px] lg:top-[317px] right-[135px] lg:right-[361px]">Seassion</p>
-    <p class="absolute top-[349px] lg:top-[352px] left-[400px] lg:left-[627px]">Student Name</p>
-    <p class="absolute top-[382px] lg:top-[387px] left-[400px] lg:left-[620px]">Father's Name</p>
-    <p class="absolute top-[412px] lg:top-[417px] left-[400px] lg:left-[613px]">Mother's Name</p>
-    <p class="absolute top-[445px] lg:top-[450px] left-[300px] lg:left-[550px]">Name of Institute</p>
-    <p class="absolute top-[479px] lg:top-[483px] left-[374px] lg:left-[600px]">244773</p>
-    <p class="absolute top-[479px] lg:top-[483px] left-[544px] lg:left-[780px]">course Name</p>
-    <p class="absolute top-[512px] lg:top-[512px] left-[474px] lg:left-[707px]">Exam Date</p>
-    <p class="absolute top-[512px] lg:top-[512px] left-[724px] lg:left-[950px]">CGPA</p>
-</div>
-</body>
-</html>
-
-
-{{--
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -60,7 +14,7 @@
 
         @font-face {
             font-family: 'Monotype Corsiva';
-            src: url('{{ asset('frontend/fonts/Monotype Corsiva/Monotype-Corsiva-Regular.ttf') }}') format('truetype');
+            src: url('{{ asset('frontend/fonts/Monotype Corsiva/Monotype-Corsiva-Regular-Italic.ttf') }}') format('truetype');
         }
 
         body {
@@ -103,20 +57,35 @@
             position: absolute;
         }
 
-        .student-id { top: 40%; left: 32%; }
-        .student-registration { top: 36%; left: 80.5%; }
-        .student-session { top: 42%; left: 80.5%; }
-        .student-name { top: 45%; left: 44.5%; }
-        .fathers-name { top: 50%; left: 43.5%; text-transform: capitalize !important; }
-        .mothers-name { top: 56.5%; left: 41.5%; text-transform: capitalize !important; }
-        .center-name { top: 58.5%; left: 40%; }
-        .student-roll { top: 62%; left: 38.5%; }
-        .student-subject { top: 63%; left: 57.5%; }
+        .student-id { top: 39%; left: 33%; }
+        .student-registration { top: 38.4%; left: 81.2%; }
+        .student-session { top: 42%; left: 81%; }
+        .student-name { top: 46.2%; left: 44.5%; }
+        .fathers-name { top: 50.5%; left: 43.5%; text-transform: capitalize  ; }
+        .mothers-name { top: 55%; left: 41.5%; text-transform: capitalize  ; }
+        .center-name { top: 59.5%; left: 40%; }
+        .student-roll { top: 64%; left: 38.5%; }
+        .student-subject { top: 64%; left: 57.5%; }
         .center-code { top: 66%; left: 87%; }
-        .exam-date { top: 69%; left: 47%; }
+        .exam-date { top: 68.5%; left: 48%; }
         .student-gpa { top: 68.5%; left: 75%; }
         .student-result-published { top: 85%; left: 24%; }
 
+        @media screen and (min-width: 740px) {
+        .student-id { top: 39%; left: 33%; }
+        .student-registration { top: 37%; left: 81%; }
+        .student-session { top: 41%; left: 80.5%; }
+        .student-name { top: 45.2%; left: 44.5%; }
+        .fathers-name { top: 50%; left: 43.5%; text-transform: capitalize !important; }
+        .mothers-name { top: 54.5%; left: 41.5%; text-transform: capitalize  ; }
+        .center-name { top: 58.5%; left: 40%; }
+        .student-roll { top: 63%; left: 38.5%; }
+        .student-subject { top: 63%; left: 57.5%; }
+        .center-code { top: 66%; left: 87%; }
+        .exam-date { top: 67.5%; left: 48%; }
+        .student-gpa { top: 67%; left: 75%; }
+        .student-result-published { top: 85%; left: 24%; }
+        }
         .no-background {
             background-image: none !important;
         }
@@ -134,7 +103,7 @@
     <div class="card-body min-h-screen" id="fullpage2">
         <div :class="{ 'no-background': !hasBackground }"
              class="back-img"
-             style="background-image:url({{ asset('images/new/certificate3.jpg') }}); position: relative; font-weight: bold;">
+             style="background-image:url({{ asset('images/student/certificate01.jpg') }}); position: relative; font-weight: bold;">
             <div>
                 <div class="absolute student-id" style="font-family: 'Segoe UI'; font-size:18px">
                     {{ \App\Lib\Helper::certificateSerialNumber($student->id) ?? '' }}
@@ -151,7 +120,7 @@
                     {{ \Carbon\Carbon::parse($student->exam_date)->format('j-F-Y') }}
                 </div>
                 <div class="absolute student-gpa">
-                    {{ $student->result->gpa() ?? '' }}
+                    {{ $student->gpa($student->result->written??0)?? '' }}
                 </div>
                 <div class="absolute student-result-published capitalize" style="font-size: 18px">
                     @if($student->result_published)
@@ -178,4 +147,4 @@
 </script>
 </body>
 </html>
---}}
+
