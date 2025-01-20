a41251bc7f49e7a762c4840c01ed70826451dd8d
shohanmia29 <shohanur7777@gmail.com>
2025-01-20 12:37:45 +0600

    fix

--- resources/views/admin/student/certificate.blade.php
+++ resources/views/admin/student/certificate.blade.php
@@ -121,7 +121,7 @@
     </style>
 </head>
 <body>
-<div x-data="{ hasBackground: true }">
+<div x-data="{ hasBackground: false }">
     <div class="w-full flex justify-end gap-1 print:hidden py-5 print:py-0">
 {{--        <button onclick="generate_pdf()" class="px-3 py-1 rounded-md bg-green-700 text-slate-100">Download</button>--}}
         <button onclick="window.print()" class="px-3 py-1 rounded-md bg-green-700 text-slate-100">Print</button>
