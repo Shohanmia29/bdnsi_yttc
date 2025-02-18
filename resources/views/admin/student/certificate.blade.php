09b9c1c0ee598a188e79f35072c188e6b874dd48
shohanmia29 <shohanur7777@gmail.com>
2025-02-18 13:47:56 +0600

    fix

--- resources/views/admin/student/certificate.blade.php
+++ resources/views/admin/student/certificate.blade.php
@@ -42,7 +42,7 @@
 
         .card-body {
             /*width: 1100px;*/
-            height: 180vh;
+            height: 170vh;
             display: flex;
             justify-content: center;
         }
