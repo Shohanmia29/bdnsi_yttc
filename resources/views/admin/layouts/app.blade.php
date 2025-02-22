1e08ba47432bdd0ce3abcf41674e889326757427
shohanmia29 <shohanur7777@gmail.com>
2025-02-22 16:34:57 +0600

    fix

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -152,6 +152,12 @@
                 >
                     <span>{{ __('Sponsor') }}</span>
                 </a>
+                <a
+                    href="{{ route('admin.translation.index') }}"
+                    class="w-full py-3 px-4 flex justify-between items-center hover:bg-slate-900 border-l-4 border-transparent hover:border-teal-400"
+                >
+                    <span>{{ __('Translation') }}</span>
+                </a>
 
                 @endrole
             </div>
