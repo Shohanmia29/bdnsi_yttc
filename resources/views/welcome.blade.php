5d2d7b32969ff0fa44e70b8076436f3746ef6923
shohanmia29 <shohanur7777@gmail.com>
2025-02-22 15:56:52 +0600

    fix center welcome  page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -445,10 +445,9 @@
                     </div>
                 </div>
             </div>
-
-            <div class="row justify-content-center school-committe-group">
+            <div class="row  justify-content-center school-committe-group">
                 @forelse(\App\Models\Center::where('status',\App\Enums\CenterStatus::Approved)->take(10)->get() as $institute)
-                    <div class="col-lg-3 col-xl-4 col-md-6 col-12">
+                    <div class="col-lg-4 col-xl-3 col-md-6 col-12">
                         <x-institute :institute="$institute"/>
                     </div>
                 @empty
@@ -457,7 +456,6 @@
 
 
             </div>
-
             <div class="row">
                 <div class="col-12">
                     <div class="section-bottom-btn">
