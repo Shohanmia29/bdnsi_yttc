86c25bb2e1566dd4e768d1e4c069aa4443642587
fxjakaria04 <freelancerx2023@gmail.com>
2025-05-12 15:14:39 +0600

    certificate done

--- resources/views/admin/student/orginalCertificate.blade.php
+++ resources/views/admin/student/orginalCertificate.blade.php
@@ -2,55 +2,67 @@
 <div class="container">
     <div class="main-box">
         <img src="{{asset('images/cetificate qr code.png')}}" alt="" class="qr-code">
-    </div>
-    <div class="box">
-        <div class="text-end">Roll No. <span class="rol-reg-sec">144773</span></div>
-        <div class="inline-block w-full">
-            <p class="float-left">Serial No. <span class="rol-reg-sec">144773</span></p>
-            <p class="float-right">Reg No. <span class="rol-reg-sec">144773</span></p>
-        </div>
-        <div class="text-end mt-2">Session. <span class="rol-reg-sec">144773</span></div>
-        <div class="cat_parent">
-            <p class="inline-block float-left text">This is to certify that</p>
-            <p class="underlined inline-block float-left">Student name</p>
-        </div>
-        <div class="cat_parent">
-            <p class="inline-block float-left text2">Son/daughter of</p>
-            <div class="underlined2 inline-block float-left">
-                <p class="float-left">Father's name</p>
-                <p class="float-right">(father)</p>
+        <div class="box">
+            <div class="text-end">Roll No. <span class="rol-reg-sec">144773</span></div>
+            <div class="inline-block w-full">
+                <p class="float-left">Serial No. <span class="rol-reg-sec">144773</span></p>
+                <p class="float-right">Reg No. <span class="rol-reg-sec">144773</span></p>
             </div>
-        </div>
-        <div class="cat_parent">
-            <p class="inline-block float-left text3">and</p>
-            <div class="underlined3 inline-block float-left">
-                <p class="float-left">Mother's name</p>
-                <p class="float-right">(mother)</p>
+            <div class="text-end mt-2">Session. <span class="rol-reg-sec">144773</span></div>
+            <div class="cat_parent">
+                <p class="inline-block float-left text">This is to certify that</p>
+                <p class="underlined inline-block float-left">Student name</p>
             </div>
-        </div>
-        <div class="cat_parent">
-            <p class="inline-block float-left text4">of</p>
-            <p class="underlined4 inline-block float-left">Institute name</p>
-        </div>
-        <div class="cat_parent">
-            <p class="inline-block float-left text5">bearing roll no</p>
-            <p class="underlined5 inline-block float-left">Roll No</p>
-            <p class="inline-block float-left text6">duly passed the</p>
-            <p class="underlined6 inline-block float-left">Institute name</p>
-        </div>
-        <div class="cat_parent">
-            <p class="inline-block float-left text7">Examination held in the month of</p>
-            <p class="underlined7 inline-block float-left">Roll No</p>
-            <p class="inline-block float-left text8">He/She scored CGPA</p>
-            <p class="underlined8 inline-block float-left">3.75</p>
-            <p class="inline-block float-left text9">on a scale of 4.00</p>
+            <div class="cat_parent">
+                <p class="inline-block float-left text2">Son/daughter of</p>
+                <div class="underlined2 inline-block float-left">
+                    <p class="float-left">Father's name</p>
+                    <p class="float-right">(father)</p>
+                </div>
+            </div>
+            <div class="cat_parent">
+                <p class="inline-block float-left text3">and</p>
+                <div class="underlined3 inline-block float-left">
+                    <p class="float-left">Mother's name</p>
+                    <p class="float-right">(mother)</p>
+                </div>
+            </div>
+            <div class="cat_parent">
+                <p class="inline-block float-left text4">of</p>
+                <p class="underlined4 inline-block float-left">Institute name</p>
+            </div>
+            <div class="cat_parent">
+                <p class="inline-block float-left text5">bearing roll no</p>
+                <p class="underlined5 inline-block float-left">244773</p>
+                <p class="inline-block float-left text6">duly passed the</p>
+                <p class="underlined6 inline-block float-left">PaikPara Model High School</p>
+            </div>
+            <div class="cat_parent">
+                <p class="inline-block float-left text7">Examination held in the month of</p>
+                <p class="underlined7 inline-block float-left">12-December-2019</p>
+                <p class="inline-block float-left text8">He/She scored CGPA</p>
+                <p class="underlined8 inline-block float-left">3.75</p>
+                <p class="inline-block float-left text9">on a scale of 4.00</p>
+            </div>
+            <p class="inline-block">at under the "Education program" A project of Young Technical Training institute</p>
         </div>
     </div>
 </div>
 <style>
+    @font-face {
+        font-family: 'Monotype Corsiva';
+        src: url('{{ asset('frontend/fonts/Monotype Corsiva/Monotype-Corsiva-Regular-Italic.ttf') }}') format('truetype');
+    }
     * {
         margin: 0;
         padding: 0;
+        font-family: Monotype Corsiva;
+    }
+    p {
+        line-height: 26px;
+    }
+    .whitespace-nowrap {
+        white-space: nowrap;
     }
     .container {
         /* min-height: 1346px; */
@@ -63,10 +75,10 @@
         width: 100%;
     }
     .text {
-        width: 140px;
+        width: 115px;
     }
     .text2 {
-        width: 110px;
+        width: 95px;
     }
     .text3 {
         width: 30px;
@@ -78,13 +90,13 @@
         width: 100px;
     }
     .text6 {
-        width: 110px;
+        width: 90px;
     }
     .text7 {
-        width: 220px;
+        width: 180px;
     }
     .text8 {
-        width: 150px;
+        width: 120px;
     }
     .text9 {
         width: 120px;
@@ -98,20 +110,20 @@
     .underlined {
         border-bottom: 2px dotted black;
         margin-bottom: 10px;
-        width: 80%;
+        width: 84%;
         padding: 0px 6px;
     }
     .underlined2 {
         border-bottom: 2px dotted black;
         margin-bottom: 10px;
-        width: 80%;
+        width: 83%;
         padding: 0px 6px;
         padding-left: 30px;
     }
     .underlined3 {
         border-bottom: 2px dotted black;
         margin-bottom: 10px;
-        width: 90%;
+        width: 91%;
         padding: 0px 6px;
         padding-left: 30px;
     }
@@ -132,7 +144,7 @@
     .underlined6 {
         border-bottom: 2px dotted black;
         margin-bottom: 10px;
-        width: 42%;
+        width: 45%;
         padding: 0px 6px;
         padding-left: 30px;
     }
@@ -146,7 +158,7 @@
     .underlined8 {
         border-bottom: 2px dotted black;
         margin-bottom: 10px;
-        width: 10%;
+        width: 18%;
         padding: 0px 6px;
         padding-left: 30px;
     }
@@ -154,8 +166,9 @@
         margin-top: 10px;
     }
     .main-box {
+        margin: 0px auto;
         position: relative;
-        min-height: 98vh;
+        min-height: 98%;
         background: url({{asset('images/certificate-background.jpg')}});
         background-size: contain;
         background-repeat: no-repeat;
@@ -164,7 +177,6 @@
     }
     .box {
         position: absolute;
-        border: 1px solid gray;
         width: 810px;
         bottom: 190px;
         left: 420px;
@@ -199,6 +211,9 @@
         .container {
             /* min-height: 558px; */
         }
+        p {
+            line-height: 20px;
+        }
         .no-print {
             display: none;
         }
@@ -217,13 +232,19 @@
             bottom: 170px;
             left: 315px;
         }
+        .text5 {
+            width: 80px;
+        }
+        .text9 {
+            width: 90px;
+        }
         .underlined {
-            width: 78%;
+            width: 81%;
         }
         .underlined2 {
             border-bottom: 2px dotted black;
             margin-bottom: 10px;
-            width: 82%;
+            width: 84%;
             padding: 0px 6px;
         }
         .underlined3 {
@@ -241,13 +262,25 @@
         .underlined5 {
             border-bottom: 2px dotted black;
             margin-bottom: 10px;
-            width: 20%;
+            width: 25%;
             padding: 0px 6px;
         }
         .underlined6 {
             border-bottom: 2px dotted black;
             margin-bottom: 10px;
-            width: 48%;
+            width: 47%;
+            padding: 0px 6px;
+        }
+        .underlined7 {
+            border-bottom: 2px dotted black;
+            margin-bottom: 10px;
+            width: 25%;
+            padding: 0px 6px;
+        }
+        .underlined8 {
+            border-bottom: 2px dotted black;
+            margin-bottom: 10px;
+            width: 14%;
             padding: 0px 6px;
         }
     }    
