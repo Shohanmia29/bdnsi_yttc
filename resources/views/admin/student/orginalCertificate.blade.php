3b3f98ad680712fe87fd2c01049cfa6c7debda8d
shohanmia29 <shohanur7777@gmail.com>
2025-07-22 06:51:32 +0600

    fix

--- resources/views/admin/student/orginalCertificate.blade.php
+++ resources/views/admin/student/orginalCertificate.blade.php
@@ -10,7 +10,7 @@
                 {{ \Carbon\Carbon::make($student->result_publised)->format('j-F-Y') }}
             @endif</p>
 
-        <p class="website">Website : {{asset('')}}</p>
+        <p class="website">Website link: www.nhmti.com</p>
 
         <div class="box">
             <p class="text-end text-lg position-0">Roll No. <span
@@ -59,7 +59,7 @@
             <div class="cat_parent">
                 <p class="inline-block float-left">of</p>
                 <p class="underlined7 inline-block float-left position-0">{{ \Carbon\Carbon::parse($student->exam_date)->format('j-F-Y') }}</p>
-                <p class="inline-block float-left">Conducted by the YTTC. He/She Secured CGPA</p>
+                <p class="inline-block float-left">Conducted by the NHMTI. He/She Secured CGPA</p>
                 <p class="underlined8 inline-block float-left position-0"> {{ number_format($student->t_written_gpa(),2)?? '' }}</p>
                 <p class="inline-block float-left">on a scale of 4.00</p>
             </div>
@@ -279,7 +279,7 @@
 
         .box {
             width: 669px;
-            bottom: 200px;
+            bottom: 197px;
             left: 633px;
         }
 
@@ -322,13 +322,13 @@
         .underlined7 {
             border-bottom: 2px dotted black!important;
             margin-bottom: 10px;
-            width: 22.5%;
+            width: 21%;
             padding-left: 35px;
         }
         .underlined8 {
             border-bottom: 2px dotted black!important;
             margin-bottom: 10px;
-            width: 11.38%;
+            width: 10.38%;
             padding: 0px 6px;
         }
         .publish_data {
